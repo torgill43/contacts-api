2db57fdd50977a00aae4dd3bd994d6693143d763
torgill43 <104460644+torgill43@users.noreply.github.com>
2023-05-10 10:47:42 -0600

    cors adjustment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         next();
     })
