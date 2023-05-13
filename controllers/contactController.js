3d18289a55c8c21f7768299449c6a5fb03981664
torgill43 <104460644+torgill43@users.noreply.github.com>
2023-05-13 15:46:02 -0600

    added try/catch to help prevent server crash

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,83 +3,102 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAllContacts = async (req, res, next) => {
     // #swagger.tags= ['Contacts']
-    const result = await mongodb.getDb().db('contacts').collection('contacts').find();
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-        // console.log(lists)
-    });
+    try {
+        const result = await mongodb.getDb().db('contacts').collection('contacts').find();
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 };
 
 const getOneContact = async (req, res, next) => {
     // #swagger.tags= ['Contacts']
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-        .getDb()
-        .db('contacts')
-        .collection('contacts')
-        .find({ _id: userId });
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-        // console.log(lists)
-    });
+    try {
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb
+            .getDb()
+            .db('contacts')
+            .collection('contacts')
+            .find({ _id: userId });
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+            // console.log(lists)
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 };
 
 const createContact = async (req, res, next) => {
     // #swagger.tags= ['Contacts']
-    console.log(req.body)
-    const contact = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
-      };
-    const result = await mongodb
-        .getDb()
-        .db('contacts')
-        .collection('contacts')
-        .insertOne(contact)
-        res.setHeader('Content-Type', 'application/json');
-        console.log(result)
-        res.status(201).json({"id":result.insertedId})
+    try {
+        console.log(req.body)
+        const contact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+        const result = await mongodb
+            .getDb()
+            .db('contacts')
+            .collection('contacts')
+            .insertOne(contact)
+            res.setHeader('Content-Type', 'application/json');
+            console.log(result)
+            res.status(201).json({"id":result.insertedId})
+    } catch {
+        res.status(500).json({ message: err.message })
+    }
 };
 
 const updateContact = async (req, res, next) => {
     // #swagger.tags= ['Contacts']
-    const userId = new ObjectId(req.params.id);
-    const contact = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
-    };
-    const result = await mongodb
-        .getDb()
-        .db('contacts')
-        .collection('contacts')
-        .updateOne({ _id: userId }, {$set: contact});
-    console.log(result)
-    if (result.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(result.error || "Some error occurred while updating the contact.")
+    try {
+        const userId = new ObjectId(req.params.id);
+        const contact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+        const result = await mongodb
+            .getDb()
+            .db('contacts')
+            .collection('contacts')
+            .updateOne({ _id: userId }, {$set: contact});
+        console.log(result)
+        if (result.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(result.error || "Some error occurred while updating the contact.")
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
     }
-};
+}
 
 const deleteContact = async (req, res, next) => {
     // #swagger.tags= ['Contacts']
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-        .getDb()
-        .db('contacts')
-        .collection('contacts')
-        .deleteOne({ _id: userId });
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send({ "message": "delete successful for _id:" + userId});
-}
+    try {
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb
+            .getDb()
+            .db('contacts')
+            .collection('contacts')
+            .deleteOne({ _id: userId });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send({ "message": "delete successful for _id:" + userId});
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+};
 
 module.exports = { 
     getAllContacts, 
